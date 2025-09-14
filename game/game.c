03a902e56933f32897dc3ae201c011f838ee3633
Kasey Clarke <KaseyDClarke@gmail.com>
2025-09-14 03:13:26 -0700

    fixed dealing cards to players

--- game/game.c
+++ game/game.c
@@ -9,17 +9,20 @@
 #include <assert.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 static game_t g_game;
 
-static bool g_dealt[TOTAL_CARDS] = {false};
+static bool g_dealt[TOTAL_CARDS];
 
 static inline void initialize_players();
+static inline void initialize_dealt();
 static inline void deal_to_players(const card_t * card_list);
 
 game_t * get_game()
 {
     initialize_players();
+    initialize_dealt();
 
     const card_t * card_list = get_master_card_list();
     deal_to_players(card_list);
@@ -28,20 +31,21 @@ game_t * get_game()
 
 }
 
-
 static inline void deal_to_players(const card_t * card_list)
 {
     for (int player_index = 0; player_index < g_game.num_players; ++player_index)
     {
 
-        int card_index;
+        int card_index = 0;
 
         for(;;)
         {
             card_index = random_number(NUM_LIGHT_CARDS);
 
-            if (false == g_dealt[card_index])
-            {                
+            if (!g_dealt[card_index])
+            {
+                printf("%2d card index\n", card_index);
+                g_dealt[card_index] = true;
                 break;
             }
         
@@ -51,7 +55,7 @@ static inline void deal_to_players(const card_t * card_list)
         card_t * p_card = (card_t *) &card_list[card_index];
 
         g_game.player_list[player_index].haul[0] = p_card;
-        
+ 
     }
 
 }
@@ -62,4 +66,12 @@ static inline void initialize_players()
     g_game.current_player = PLAYER_1;
     g_game.player_list = get_player_list();
 
+}
+
+static inline void initialize_dealt()
+{
+    for (int index = 0; index < TOTAL_CARDS; ++index)
+    {
+        g_dealt[index] = false;
+    }
 }
