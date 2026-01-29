659036a10f4bc632fd8998a2dab0eacba1d31042
Kasey Clarke <KaseyDClarke@gmail.com>
2026-01-29 09:33:30 -0800

    cleaned up player.h

--- players/players.c
+++ players/players.c
@@ -9,16 +9,14 @@
 static player_t g_players[MAX_PLAYERS];
 
 static inline void initialize_players();
-static inline void initialize_suit_counts();
-static inline void initialize_value_counts();
+static inline void initialize_suit_and_value_counts_to_zero();
 static inline void initialize_hauls();
 static inline void initialize_awards();
 
 player_t * get_player_list()
 {
     initialize_players();
-    initialize_suit_counts();
-    initialize_value_counts();
+    initialize_suit_and_value_counts_to_zero();
     initialize_hauls();
     initialize_awards();
     
@@ -27,64 +25,63 @@ player_t * get_player_list()
 
 static inline void initialize_awards()
 {
-    for (int index = 0; index < MAX_PLAYERS; ++index)
+    for (int player_count = 0; player_count < MAX_PLAYERS; ++player_count)
     {
         for(int tiles = 0; tiles < NUM_SCORING_TILES; ++tiles)
         {
-            g_players[index].awards[tiles] = false;
+            g_players[player_count].awards[tiles] = false;
         }
     }
-}
-
-static inline void initialize_suit_counts()
-{
-    for (int player_index = 0; player_index < MAX_PLAYERS; ++player_index)
-    {
-        for (int suit_index = 0; suit_index < NUM_BASIC_SUITS; ++suit_index)
-        {
-            g_players[player_index].num_suits[suit_index] = 0;
-        }
-
-        for (int value = THREES; value <= FIVES; ++value)
-        {
-            g_players[player_index].num_values[value] = 0;
-        }
-    
-
-    }
-
 }
 
 
 static inline void initialize_players()
 {
-    int player_number = 0;
-    for(int index = 0; index < MAX_PLAYERS; ++index)
+    for(int player_count = 0; player_count < MAX_PLAYERS; ++player_count)
     {
-        g_players[index].num_curses = 0;
-        g_players[index].exit_number = 0;
-        g_players[index].cards_in_haul = 0;
-        g_players[index].points = 0;
+        g_players[player_count].num_curses = 0;
+        g_players[player_count].exit_number = 0;
+        g_players[player_count].cards_in_haul = 0;
+        g_players[player_count].points = 0;
+
+        g_players[player_count].in_tower = false;
+        g_players[player_count].has_exited = false;
+        g_players[player_count].is_you = false;
+        g_players[player_count].first_card_revealed = false;
         
-        g_players[index].in_tower = false;
-        g_players[index].has_exited = false;
-        g_players[index].is_you = false;
-        g_players[index].first_card_revealed = false;
-        
-        g_players[index].player_number = player_number;
-        ++player_number;
+        g_players[player_count].player_number = player_count;
 
     }
     
 }
 
+
+static inline void initialize_suit_and_value_counts_to_zero()
+{
+    for (int player_count = 0; player_count < MAX_PLAYERS; ++player_count)
+    {
+
+        for (int suit_count = 0; suit_count < NUM_BASIC_SUITS; ++suit_count)
+        {
+            g_players[player_count].num_suits[suit_count] = 0;
+        }
+
+        for (int value_count = THREES; value_count <= FIVES; ++value_count)
+        {
+            g_players[player_count].num_values[value_count] = 0;
+        }
+
+    }
+
+}
+
 static inline void initialize_hauls()
 {
-    for(int index = 0; index < MAX_PLAYERS; ++index)
+    for(int player_count = 0; player_count < MAX_PLAYERS; ++player_count)
     {
-        for(int haul_index = 0; haul_index < MAX_HAUL; ++haul_index)
+        for(int haul_player_count = 0; haul_player_count < MAX_HAUL; ++haul_player_count)
         {
-            g_players[index].haul[haul_index] = NULL;
+            g_players[player_count].haul[haul_player_count] = NULL;
         }
     }
 
